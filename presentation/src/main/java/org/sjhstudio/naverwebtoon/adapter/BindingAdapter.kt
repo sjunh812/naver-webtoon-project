4d57c185f3ba3316b7250e02e0c5de3b9642e9c4
sjunh812 <sjunh812@naver.com>
2022-10-16 21:15:36 +0900

    feat: weekday list
    - New webtoon list animation..

--- presentation/src/main/java/org/sjhstudio/naverwebtoon/adapter/BindingAdapter.kt
+++ presentation/src/main/java/org/sjhstudio/naverwebtoon/adapter/BindingAdapter.kt
@@ -1,19 +1,52 @@
 package org.sjhstudio.naverwebtoon.adapter
 
-import android.view.View
+import android.graphics.drawable.Drawable
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import org.sjhstudio.naverwebtoon.R
 
 @BindingAdapter("imageFromUrl")
 fun ImageView.bindImageFromUrl(url: String?) {
     if (!url.isNullOrEmpty()) {
-        visibility = View.VISIBLE
         Glide.with(context)
             .load(url)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    if (url.contains("backImage") && !isVisible) {
+                        isVisible = true
+                        startAnimation(AnimationUtils.loadAnimation(context, R.anim.back_thumbnail))
+                    } else if (url.contains("frontImage") && !isVisible) {
+                        isVisible = true
+                        startAnimation(AnimationUtils.loadAnimation(context, R.anim.front_thumbnail))
+                    }
+                    return false
+                }
+            })
             .into(this)
-    } else visibility = View.GONE
+    } else setImageResource(0)
 }
 
 @BindingAdapter("adapter")
